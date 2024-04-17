d228a25b4a96162ccf152d4f465e2148cab0c2e5
toeten <n.santiago0228@gmail.com>
2024-04-17 15:22:42 -0400

    answered question 6

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -18,7 +18,9 @@ const hasPunctuationEnd = (str) => {
   return /[.!?]$/.test(str)
 };
 
-const hasNothingOrDigits = (str) => { };
+const hasNothingOrDigits = (str) => {
+  return /^(\s|\d)*$/gi.test(str)
+};
 
 const hasNoFlippers = (str) => { };
 
