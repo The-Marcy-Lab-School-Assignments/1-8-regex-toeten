6b0985707d090588e8a5781e8087a1269310b685
toeten <n.santiago0228@gmail.com>
2024-04-18 09:20:08 -0400

    answered question 17

--- src/debug.js
+++ src/debug.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars, no-useless-escape */
 
 const isValidCompanyUsername = (username, firstName, lastName) => {
-  const regex = new RegExp(`^(sales|tech)\d\w-${firstName[0]}${lastName}\d?$`);
-  // console.log(regex) // to help you debug, look at the final regex
+  const regex = new RegExp(`^(sales|tech)\\d\\w\\-${firstName[0]}${lastName}\\d?$`);
+  //console.log(regex) // to help you debug, look at the final regex
   return regex.test(username);
 };
 
 module.exports = {
   isValidCompanyUsername,
 };
+
+isValidCompanyUsername('sales9b-ajohnson1', 'albert', 'johnson')
